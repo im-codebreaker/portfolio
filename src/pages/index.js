224b8b721e56b7bb414aa5fa3680040ff14e034b
MehdiHan <meh.bouchard@outlook.fr>
2022-06-09 08:07:14 +0200

    chore: add gsap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,42 @@
 import * as React from 'react';
+import gsap from 'gsap';
 import { Link } from 'gatsby';
 import { Header, Footer } from '../layout';
 import '../sass/main.scss';
 import './index.scss';
 
 const IndexPage = () => {
+
+  const element = React.useRef();
+  const q = gsap.utils.selector(element);
+  const timeline =  React.useRef();
+
+  React.useEffect(()=>{
+    timeline.current = gsap.timeline()
+      .to(q('.hero_baseline-title'),{
+        ease:"power4.inOut",
+        y:0,
+        opacity:1,
+        'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+        duration:2
+      })
+      .to(q('.section_hero-image'),{
+        ease: "elastic.out(1, 0.3)",
+        y:0,
+        opacity:1,
+        duration:2
+      })
+      .to(q('.baseline_title-button'),{
+        opacity:1,
+        duration:1
+      })      
+  },[]);
   return (
     <>
     <Header />   
       <main className='container'>
         <section className='section_hero'>
-          <div className='section_hero-wrapper'>
+          <div className='section_hero-wrapper' ref={element}>
             <div className='section_hero-baseline'>
               <h1 className='hero_baseline-title'>
                 <span className='greeting'>Hello Wolrd,</span>
